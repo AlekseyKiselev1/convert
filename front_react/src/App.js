fee3a2ca05a0882298954dfa892d0696ddb06198
AlekseyKiselev1 <arkadivich1990@gmail.com>
2025-04-04 03:35:47 +0300

    добавление изоображений на фон

--- front_react/src/App.js
+++ front_react/src/App.js
@@ -1,10 +1,17 @@
+import React from "react";
 import CurrencyConverter from "./components/CurrencyConverter";
-import "./App.css"; 
+import "./App.css";
+import bitcoin from "./images/bitcoin.png";
+import ethereum from "./images/ethereum.png";
 
 function App() {
   return (
     <div className="App">
+      <img src={bitcoin} alt="Bitcoin" className="top-left-image" />
+      
       <CurrencyConverter />
+
+      <img src={ethereum} alt="Ethereum" className="bottom-right-image" />
     </div>
   );
 }
